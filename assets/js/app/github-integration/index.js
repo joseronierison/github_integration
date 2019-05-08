8f1f1c466088c4f87945abb742b739d2d1b3f976
joseronierison <ronierison.silva@gmail.com>
2019-05-07 22:22:45 -0300

    Remove page base

--- assets/js/app/github-integration/index.js
+++ assets/js/app/github-integration/index.js
@@ -2,12 +2,10 @@ import HomeNavbar from './components/HomeNavbar';
 import RepositoryList from './components/RepositoryList';
 import Loading from './components/Loading';
 import CommitList from './components/CommitList';
-import PageBase from './components/PageBase';
 
 export {
   HomeNavbar,
   RepositoryList,
   Loading,
   CommitList,
-  PageBase,
 };
