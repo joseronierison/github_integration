804ee3f74e39a39e0b355346358afb45af94e893
joseronierison <ronierison.silva@gmail.com>
2019-05-12 12:57:42 -0300

    Add test for User model

---
+++ users/tests/test_models.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from users.models import User
+
+class UserTestCase(TestCase):
+    email = "test@@test.com"
+    user = User(email=email)
+
+    def test_str_should_return_email(self):
+        self.assertEqual(str(self.user), self.email)
+
+    def test_get_full_name_should_return_email(self):
+        self.assertEqual(self.user.get_full_name(), self.email)
+
+    def test_get_short_name_should_return_email(self):
+        self.assertEqual(self.user.get_short_name(), self.email)
